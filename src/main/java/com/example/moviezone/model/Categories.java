2463d333eaba0a5b40a22d2b0d6743974f479e83
DenicaKj <dkorvezir@gmail.com>
2023-02-02 14:24:11 +0100

    Added Plugin
    added plugin

---
+++ src/main/java/com/example/moviezone/model/Categories.java
@@ -0,0 +1,13 @@
+package com.example.moviezone.model;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Categories {
+}
