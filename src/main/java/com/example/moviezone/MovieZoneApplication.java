1bcb2a836fb4eec3e67695abebfc593735071a34
DenicaKj <dkorvezir@gmail.com>
2023-02-02 13:11:37 +0100

    Initial commit

---
+++ src/main/java/com/example/moviezone/MovieZoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieZoneApplication.class, args);
+    }
+
+}
