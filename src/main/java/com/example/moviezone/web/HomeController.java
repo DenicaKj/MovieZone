adf250d1e763063d4f3057443205bed4387ca64a
DenicaKj <dkorvezir@gmail.com>
2023-02-04 18:45:35 +0100

    Update HomeController.java

--- src/main/java/com/example/moviezone/web/HomeController.java
+++ src/main/java/com/example/moviezone/web/HomeController.java
@@ -3,6 +3,8 @@ package com.example.moviezone.web;
 
 import com.example.moviezone.service.FilmService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +15,12 @@ private final FilmService filmService;
 
     public HomeController(FilmService filmService) {
         this.filmService = filmService;
+    }
+
+    @GetMapping
+    public String getHomePage(Model model) {
+        model.addAttribute("bodyContent", "home");
+        return "master-template";
     }
 
 
