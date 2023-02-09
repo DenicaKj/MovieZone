6a9006dec88a98ac7e98a4ed1ae5437388b900a6
milamihajlovska <mila.mihajlovska01@hotmail.co.uk>
2023-02-09 14:37:25 +0100

    update userNotFoundException

--- src/main/java/com/example/moviezone/model/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/moviezone/model/exceptions/UserNotFoundException.java
@@ -2,5 +2,7 @@ package com.example.moviezone.model.exceptions;
 
 
 public class UserNotFoundException extends RuntimeException{
-
+    public UserNotFoundException() {
+        super("user not found");
+    }
 }
