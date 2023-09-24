c0baf6eec9033fb8f8bb6ec547352b3b79da39fd
DenicaKj <dkorvezir@gmail.com>
2023-09-24 04:54:27 +0200

    added repository tests

---
+++ src/test/java/com/example/moviezone/TicketRepositoryTest.java
@@ -0,0 +1,82 @@
+package com.example.moviezone;
+
+import com.example.moviezone.model.*;
+import com.example.moviezone.repository.TicketRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase
+public class TicketRepositoryTest {
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @Test
+    public void testSaveTicket() {
+        // Create a Ticket
+        Ticket ticket = new Ticket(1000L, LocalDate.now(), new Customer(), new Projection(), new Discount(), new Seat());
+
+        // Save the Ticket
+        Ticket savedTicket = ticketRepository.save(ticket);
+
+        // Verify that the Ticket was saved with an ID
+        assertNotNull(savedTicket.getId_ticket());
+    }
+
+    @Test
+    public void testFindTicketById() {
+        // Create a Ticket and save it
+        Ticket ticket = new Ticket(1000L, LocalDate.now(), new Customer(), new Projection(), new Discount(), new Seat());
+        ticket = ticketRepository.save(ticket);
+
+        // Find the Ticket by ID
+        Ticket foundTicket = ticketRepository.findById(ticket.getId_ticket()).orElse(null);
+
+        // Verify that the Ticket was found
+        assertNotNull(foundTicket);
+        assertEquals(ticket.getId_ticket(), foundTicket.getId_ticket());
+    }
+
+    @Test
+    public void testUpdateTicket() {
+        // Create a Ticket and save it
+        Ticket ticket = new Ticket(1000L, LocalDate.now(), new Customer(), new Projection(), new Discount(), new Seat());
+        ticket = ticketRepository.save(ticket);
+
+        // Update the Ticket's price
+        ticket.setPrice(1200L);
+        ticket = ticketRepository.save(ticket);
+
+        // Find the updated Ticket by ID
+        Ticket updatedTicket = ticketRepository.findById(ticket.getId_ticket()).orElse(null);
+
+        // Verify that the Ticket was updated
+        assertNotNull(updatedTicket);
+        assertEquals(1200L, updatedTicket.getPrice());
+    }
+
+    @Test
+    public void testDeleteTicket() {
+        // Create a Ticket and save it
+        Ticket ticket = new Ticket(1000L, LocalDate.now(), new Customer(), new Projection(), new Discount(), new Seat());
+        ticket = ticketRepository.save(ticket);
+
+        // Delete the Ticket
+        ticketRepository.delete(ticket);
+
+        // Try to find the deleted Ticket by ID
+        Ticket deletedTicket = ticketRepository.findById(ticket.getId_ticket()).orElse(null);
+
+        // Verify that the Ticket was deleted
+        assertNull(deletedTicket);
+    }
+}
---
+++ src/test/java/com/example/moviezone/UserRepositoryTest.java
@@ -0,0 +1,95 @@
+package com.example.moviezone;
+
+import com.example.moviezone.model.User;
+import com.example.moviezone.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testSaveUser() {
+        // Create a User entity
+        User user = new User();
+        user.setUsername("testuser");
+        user.setAddress("test@example.com");
+
+        // Save the user to the database
+        User savedUser = userRepository.save(user);
+
+        // Assert that the user was saved and has an ID
+        Assertions.assertNotNull(savedUser.getId_user());
+    }
+
+    @Test
+    public void testFindUserByUsername() {
+        // Save a user to the database
+        User user = new User();
+        user.setUsername("testuser");
+        user.setAddress("test@example.com");
+        userRepository.save(user);
+
+        // Retrieve the user by username
+        Optional<User> retrievedUser = Optional.ofNullable(userRepository.findByUsername("testuser"));
+
+        // Assert that the retrieved user matches the saved user
+        assertTrue(retrievedUser.isPresent());
+        assertEquals("test@example.com", retrievedUser.get().getAddress());
+    }
+
+    @Test
+    public void testDeleteUser() {
+        // Save a user to the database
+        User user = new User();
+        user.setUsername("testuser");
+        user.setAddress("test@example.com");
+        userRepository.save(user);
+
+        // Delete the user from the database
+        userRepository.delete(user);
+
+        // Assert that the user is no longer in the database
+        assertNull(userRepository.findByUsername("testuser"));
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
+    public void testUserCount() {
+        // Save three users to the database
+        User user1 = new User();
+        user1.setUsername("user1");
+        user1.setAddress("user1@example.com");
+        userRepository.save(user1);
+
+        User user2 = new User();
+        user2.setUsername("user2");
+        user2.setAddress("user2@example.com");
+        userRepository.save(user2);
+
+        User user3 = new User();
+        user3.setUsername("user3");
+        user3.setAddress("user3@example.com");
+        userRepository.save(user3);
+
+        // Assert that the count of users in the database is 3
+        assertEquals(3, userRepository.count());
+    }
+
+    // Add more test methods to cover other repository operations
+}
